007ea621b9f11cd981681ec1badc772d715c7acd
Cesar Redondo Bueno <revuls@gmail.com>
2025-06-15 13:58:45 +0200

    feat: change vite config and add luminis icon

--- vite.config.js
+++ vite.config.js
@@ -1,52 +1,41 @@
 import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
 
+// vite.config.js
 export default defineConfig({
-  // Configuración de la raíz del proyecto
-  root: 'src', // Directorio donde está tu código fuente (en este caso, la carpeta src)
-
-  // Configuración de la salida de la compilación
+  root: 'src',
+  publicDir: '../public',
   build: {
-    outDir: '../dist', // Carpeta donde se generará el código compilado
+    outDir: '../dist',
     rollupOptions: {
-      input: 'src/index.html', // El archivo HTML principal para Vite
+      input: 'src/index.html',
     },
   },
-
-  // Configuración del servidor de desarrollo
   server: {
-    open: true, // Abrir automáticamente el navegador cuando inicie el servidor
-    port: 3000, // Puerto donde se ejecutará el servidor
+    open: true,
+    port: 3000,
   },
-  // Configuración de VitePWA para la generación de PWA
-  // Puedes personalizar la configuración de VitePWA según tus necesidades
   plugins: [
     VitePWA({
       registerType: 'autoUpdate',
       includeAssets: ['favicon.ico', 'robots.txt'],
       manifest: {
-        name: 'Mi App Lit PWA',
-        short_name: 'LitApp',
-        start_url: '.',
+        id: '/',
+        name: 'Mi Tutor',
+        short_name: 'Tutor',
+        start_url: '/',
+        scope: '/',
         display: 'standalone',
         background_color: '#ffffff',
         theme_color: '#0078d7',
         icons: [
           {
-            src: 'assets/icons/icon-192.png',
+            src: '/icons/icon-192.png',
             sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: 'assets/icons/icon-512.png',
-            sizes: '512x512',
             type: 'image/png',
           },
         ],
       },
     }),
   ],
-
-  // Si tienes que manejar recursos estáticos o imágenes, puedes configurar la carpeta de assets
-  publicDir: 'src/assets', // Directorio donde se guardan los recursos públicos como imágenes, fuentes, etc.
 });
